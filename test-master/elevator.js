f10c0fc567aa0e1d51f4346665ee6bed32747d6f
1208steve <90750531+1208steve@users.noreply.github.com>
2022-01-12 13:36:53 +0800

    SSA

--- test-master/elevator.js
+++ test-master/elevator.js
@@ -1,9 +1,9 @@
-var floorName = []; //用來儲存樓層的陣列
-var MaxF = 100;     //最大樓層
-var MinF = -3;      //最小樓層
+var floorName = []; 
+var MaxF = 100;     
+var MinF = -3;      
 
 
-//用陣列新增樓層
+
 for (var i = MinF; i < MaxF + 1; i++){
     if(i < 0){
         floorName.push("B" + i * -1 );
@@ -12,27 +12,8 @@ for (var i = MinF; i < MaxF + 1; i++){
     }
 }
 
-console.log(floorName.join(", "));//輸出樓層
+console.log(floorName.join(", "));
 
-
-//延遲
-//方法一 寫一個function
-//用getTime取得時間後持續計算時間 直到時間到才跳出迴圈
-//時間到之前 以下的程式碼都不執行
-function Dealy1(sec){
-    var start = new Date().getTime();
-    var now = 0;
-    while(start + sec > now){
-        now = new Date().getTime();
-    };
-}
-Dealy1(3000);
-console.log("Dealy1");
-
-
-//方法二 用setTimeout
-//必須使用let或直接新開一個function把當時在for中的i儲存起來
-//時間到之前會繼續往後執行
 for (let i = 0; i < 5; i++){
     setTimeout(() => {console.log(i)}, 4000);
 }
